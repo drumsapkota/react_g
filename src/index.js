b341dccee6b513afab94659807fc5077e3ae84b3
Adi <drumsapkota@gmail.com>
2017-05-25 17:09:31 +0530

    The github card app with Github integration and axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import axios from 'axios';
 
 const Card = (props) => {
   return(
@@ -16,26 +16,59 @@ const Card = (props) => {
     );
 };
 
-let data= [
-  {
-    name:"Paul O'Shannessy",
-    avatar_url:"https://avatars2.githubusercontent.com/u/8445?v=3",
-    company:"Facebook"
-  },
-  {
-    name:"Ben Alpert",
-    avatar_url:"https://avatars1.githubusercontent.com/u/6820?v=3",
-    company:"Facebook"
-  }
-]
+
 
 const CardList = (props) => {
   return(
     <div>
-      {props.cards.map(card => <Card {...card} />)}
+      {props.cards.map(card => <Card key={card.id} {...card} />)}
     </div>
     );
 }
 
-ReactDOM.render(<CardList cards={data}/>, document.getElementById('root'));
+class Form extends React.Component{
+	state={userName:''}
+	handleSubmit =(event) => {
+		event.preventDefault();
+		console.log("Form works!!", this.state.userName);
+		axios.get(`https://api.github.com/users/${this.state.userName}`)
+			.then(resp => {
+				this.props.onSubmit(resp.data);
+				this.setState({userName:''});
+			});
+	}
+	render(){
+		return(
+			<form onSubmit={this.handleSubmit}>
+				<input type="text" 
+				placeholder="Github username" 
+				onChange={(event) => this.setState({userName: event.target.value})}
+				required/>
+				<button type= "submit">Add card </button>
+			</form>
+			);
+	}
+}
+
+class App extends React.Component{
+	state={
+	cards:	[]
+	};
+	addNewCard = (cardInfo) =>{
+		this.setState(prevState => ({
+			cards:prevState.cards.concat(cardInfo)
+		}));
+	};
+
+	render(){
+		return(
+			<div>
+				<Form onSubmit={this.addNewCard}/>
+				<CardList cards={this.state.cards} />
+			</div>
+			)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
